30bb29462e216e03427f1e7496b63a434a687917
Mohammed Arfan <mdarfan357@gmail.com>
2022-11-26 22:20:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-import requests
-import json
-from streamlit_lottie import st_lottie
+import edgenum as en
+import datetime as ts
+
+if "func_out" not in st.session_state:
+    st.session_state.func_out = en.edgenum("C:/Users/rgkul/Downloads/images/g7.jpg")
+
+
 st.title("Automated Parking System using number plate detection")
 hide_streamlit_style = """
             <style>
@@ -10,29 +14,9 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-def load_lottieurl(url):
-  r = requests.get(url)
-  if r.status_code != 200:
-    return None
-  return r.json()
-
-lottie_hello = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_khm3kzeu.json")
-            
-
-st_lottie(
-    lottie_hello,
-    speed=0.75,
-    reverse=False,
-    loop=False,
-    quality="high", # medium ; high
-  
-    height=None,
-    width=None,
-    key=None,
-)
 
 if "button_clicked" not in st.session_state:
-    st.session_state.button_clicked = False
+    st.session_state.button_clicked1 = False
 
 # if "button_clicked2" not in st.session_state:    
 #     st.session_state.button_clicked2 = False
@@ -42,25 +26,46 @@ enter = st.button("Enter")
 def callback():
     st.session_state.button_clicked =True
 
+# def callback2():
+#     st.session_state.button_clicked =True
+output = st.session_state.func_out
+# output = en.edgenum("C:/Users/rgkul/Downloads/images/g7.jpg")
 
 if enter:
     _left,left,l0,l1,l2, mid,_right = st.columns(7)
     with left:
-        st.image("Cars428.png",width=500)
+        st.image("images/g7.jpg",width=500)
     import time
 
     my_bar = st.progress(0)
-    st.info('Model is running ', icon="ℹ️")
+    
     for percent_complete in range(100):
         time.sleep(0.001)
         my_bar.progress(percent_complete + 1)
-    
-    
+    st.info('Vehicle entry registered, press exit to simulate vehicle exit ', icon="ℹ️")
 
+dt1 = ts.datetime(2022, 11, 26, 12, 25, 29, 674107)    
+try:
+    timedf = output[0]-dt1
+except:
+    pass
+# append_to_csv(tweet[0],tweet[1])
+
+# tweet - date time and car plate
+diff_in_minutes = timedf.total_seconds() / 60
+if diff_in_minutes <= 60:
+    Amount = 20
+elif diff_in_minutes > 60 and diff_in_minutes <= 120:
+    amount = 30
+elif diff_in_minutes > 120 and diff_in_minutes <= 180:
+    amount = 40
+elif diff_in_minutes > 180:
+    amount = 50
 # exit = st.button("Exit")
 
 if st.button("Exit",on_click=callback):
-    st.write(f"Payment amount : x Rs")
+
+    st.markdown(f"**Payment amount for vehicle number {output[1]} : {amount} Rs for {round(diff_in_minutes/60,2)} hours.**")
     st.write(f"Payment QR code :")
     _left,l1, mid,m1, _right,r1,r2 = st.columns(7)
     with mid:
@@ -70,6 +75,7 @@ if st.button("Exit",on_click=callback):
     pay = st.button("Make payment")
     # print(pay)
     if pay:
+
         st.success("Payment Successful")
 
     # accept = st.button("Accept")
