cf6c8459948b4921ff013f14badb8a1cc3e204ee
Mohammed Arfan <mdarfan357@gmail.com>
2022-11-27 08:44:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import datetime as ts
 import requests
 import json
 from streamlit_lottie import st_lottie
-IMAGE_PATH = "g9.png" # "g7.jpg" # "g8.jpg"
+# IMAGE_PATH = "g9.png" # "g7.jpg" # "g8.jpg"
+IMAGE_PATH = "g10.webp"
 
 
 if "func_out" not in st.session_state:
